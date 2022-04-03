a23bd3083e37acd5d6c85a0d3d8add1e0436d937
Assem <ihab@fuinco.com>
2022-04-03 14:08:25 +0200

    first task in fuinco

---
+++ src/main/java/com/example/backEndTask/vehicle/VehicleEntity.java
@@ -0,0 +1,23 @@
+package com.example.backEndTask.vehicle;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "vehicle")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class VehicleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    private String name;
+    @Column(nullable = false,unique = true)
+    private String serialNum;
+}
